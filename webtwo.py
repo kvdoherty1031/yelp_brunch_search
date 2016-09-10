7b81cb4caecb5cd204d37f2860bcb321f348bcb2
kvdoherty1031 <kvdoherty@gmail.com>
2016-09-10 16:44:29 -0400

    update for heroku

--- webtwo.py
+++ webtwo.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from yelpAPITwo import *
+import os
 app = Flask(__name__)
 
 
@@ -15,4 +16,5 @@ def about():
 	return render_template("abouttwo.html")
 
 if __name__ == "__main__":
-	app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0.", port=port)
